89291bf44168f87ca456e42c735bb1a66283adab
amandamg <amgarcia2996@gmail.com>
2018-09-01 03:00:54 -0700

    added user authenication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,15 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    permit_attrs(%i[firstName lastName bio profilePic favQuote specialty years_exp])
+  end
+
+  def permit_attrs(attrs)
+    %i[sign_up account_update].each do |action|
+      devise_parameter_sanitizer.permit(action, keys: attrs)
+    end
+  end
 end
